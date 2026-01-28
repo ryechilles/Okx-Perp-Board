865edb47b71a9254a881c70cd2fd0d3f4c60f6e7
ryechilles <fuyupinggg@gmail.com>
2026-01-28 14:35:54 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 静态部署到 Vercel / GitHub 都 OK（默认）
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
