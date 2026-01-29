8813cd4d2d2ee8c0e3fba87866478917fcb5532d
ryechilles <fuyupinggg@gmail.com>
2026-01-29 16:23:54 +0800

    Delete components directory

--- components/TableHeader.tsx
+++
@@ -1,5 +0,0 @@
-// TableHeader is now integrated into PerpBoard.tsx
-// This file is kept for compatibility but not used
-export function TableHeader() {
-  return null;
-}
--- components/TableRow.tsx
+++
@@ -1,5 +0,0 @@
-// TableRow is now integrated into PerpBoard.tsx
-// This file is kept for compatibility but not used
-export function TableRow() {
-  return null;
-}
