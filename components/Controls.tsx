8e07b6c17286d237f656c171a6794db956cabb24
ryechilles <fuyupinggg@gmail.com>
2026-01-31 13:36:33 +0800

    Update Controls.tsx

--- components/Controls.tsx
+++ components/Controls.tsx
@@ -425,7 +425,7 @@ export function Controls({
       {/* Sidebar Panel */}
       <div
         ref={customizePanelRef}
-        className={`fixed top-0 right-0 h-full w-[360px] bg-white shadow-xl z-[101] transform transition-transform duration-300 ease-in-out ${
+        className={`fixed top-0 right-0 h-full w-[420px] bg-white shadow-xl z-[101] transform transition-transform duration-300 ease-in-out ${
           showCustomizePanel ? 'translate-x-0' : 'translate-x-full'
         }`}
       >
