bd24d448b3966ddf4c6dc3795843e6b7d41377d5
ryechilles <fuyupinggg@gmail.com>
2026-02-03 09:25:30 +0800

    Update BTCDominance.tsx

--- components/BTCDominance.tsx
+++ components/BTCDominance.tsx
@@ -8,39 +8,22 @@ export function BTCDominance() {
   useEffect(() => {
     if (!containerRef.current) return;
 
-    // Clear any existing content
-    containerRef.current.innerHTML = '';
-
-    // Create TradingView Mini Symbol Overview widget
+    // Load TradingView script
     const script = document.createElement('script');
-    script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-mini-symbol-overview.js';
-    script.type = 'text/javascript';
-    script.async = true;
-    script.innerHTML = JSON.stringify({
-      symbol: "CRYPTOCAP:BTC.D",
-      width: "100%",
-      height: "100%",
-      locale: "en",
-      dateRange: "1D",
-      colorTheme: "light",
-      isTransparent: true,
-      autosize: true,
-      largeChartUrl: ""
-    });
-
-    containerRef.current.appendChild(script);
+    script.type = 'module';
+    script.src = 'https://widgets.tradingview-widget.com/w/en/tv-mini-chart.js';
+    document.head.appendChild(script);
 
     return () => {
-      if (containerRef.current) {
-        containerRef.current.innerHTML = '';
-      }
+      // Cleanup script on unmount
+      script.remove();
     };
   }, []);
 
   return (
-    <div
-      ref={containerRef}
-      className="tradingview-widget-container"
-    />
+    <div ref={containerRef}>
+      {/* @ts-ignore - TradingView Web Component */}
+      <tv-mini-chart symbol="CRYPTOCAP:BTC.D"></tv-mini-chart>
+    </div>
   );
 }
